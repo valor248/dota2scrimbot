43b12e21d86f851899e7d0f3ab9af120dc7f55f5
Benji Altman <baltman@sabgrp.com>
2020-09-08 11:36:38 -0500

    to typescript kinda working

--- index.old.ts
+++ index.old.ts
@@ -10,10 +10,16 @@ const scrimPlayer = '749740102569295902';
 
 const bot = new Discord.Client();
 
-let scrimPlayerListObject = [];
+interface ScrimPlayer{
+	Name: string;
+	Roles: number[];
+	MMR: number;
+}
 
-bot.on('message', (message) => {
-	let parts = message.content.split(' ');
+let scrimPlayerListObject: ScrimPlayer[] = [];
+
+bot.on('message', (message: any) => {
+	let parts: string[] = message.content.split(' ');
 
 	// !signin Command. Used to push a user's name, roles, and MMR to scrimPlayerList
 	// Can be used by any user with access to allowed channels.
@@ -22,13 +28,11 @@ bot.on('message', (message) => {
 			message.reply("Your message was incorrectly formatted.\n`!signin {first_choice_role,second_choice_role,...} {mmr}`");
 			return;
 		}
-		let roles = parts[1].toString();
-		roles = roles.split(',');
-		let mmr = parts[2];
-		mmr = parseInt(mmr, 10);
+		let roles = parts[1].split(',').map(x => +x);
+		let mmr = +parts[2];
 		let rolesValid = rolesValidCheck(roles, message);
 		let mmrValid = mmrValidCheck(mmr, message);
-        let username = message.member.user.tag;
+        let username: string = message.member.user.tag;
         username = username.substring(0, username.indexOf('#'));
 		let isSignedUp = isSignedUpCheck(username, message);
 		if (rolesValid && mmrValid && isSignedUp) {
@@ -47,7 +51,6 @@ bot.on('message', (message) => {
 
 		if (message.member.hasPermission("BAN_MEMBERS")) {
             scrimPlayerListObject = [];
-			scrimPlayerList = `Scrim Player List: \n`;
 			message.reply(`The Scrim Player List has been cleared.`);
 		} else {
             message.reply(`You do not have permission to use this command.`);
@@ -59,10 +62,8 @@ bot.on('message', (message) => {
                 message.reply("Your message was incorrectly formatted.\n`!signinManual {name} {first_choice_role,second_choice_role,...} {mmr}`");
                 return;
             }
-            let roles = parts[2].toString();
-            roles = roles.split(',');
-            let mmr = parts[3];
-            mmr = parseInt(mmr, 10);
+            let roles = parts[2].split(',').map(x => +x);
+            let mmr = +parts[3];
             let rolesValid = rolesValidCheck(roles, message);
             let mmrValid = mmrValidCheck(mmr, message);
             let username = parts[1];
@@ -108,7 +109,7 @@ bot.on('message', (message) => {
 bot.login(Tokens ? Tokens.DISCORD : process.env.DISCORD_TOKEN);
 
 
-function rolesValidCheck(rolesArray, message) {
+function rolesValidCheck(rolesArray: number[], message: any) {
 	if (rolesArray.length <= 1) {
 		message.reply('Make sure you select more than one role. (e.g. 1,2)');
 		return false;
@@ -124,7 +125,7 @@ function rolesValidCheck(rolesArray, message) {
 	return true;
 }
 
-function mmrValidCheck(mmrNumber, message) {
+function mmrValidCheck(mmrNumber: any, message: any) {
 	if (isNaN(mmrNumber)) {
 		message.reply('Make sure your mmr is a number. (e.g. 4000)');
 		return false;
@@ -132,7 +133,7 @@ function mmrValidCheck(mmrNumber, message) {
 	return true;
 }
 
-function isSignedUpCheck(username, message) {
+function isSignedUpCheck(username: any, message: any) {
 	if (scrimPlayerListObject.find(x => x.Name === username)) {
 		message.reply('You are already signed in. Sign out and sign back in to change settings.');
 		return false;
@@ -144,7 +145,7 @@ function isSignedUpCheck(username, message) {
  * gets a pretty ascii table
  * @param {string[][]} table 
  */
-function prettyPrintTable(table, columnDelimeter = '   ')
+function prettyPrintTable(table: string[][], columnDelimeter = '   '): string
 {
 	const maxLength = table[0].map(x => x.length);
 
@@ -161,13 +162,9 @@ function prettyPrintTable(table, columnDelimeter = '   ')
 
 	const paddedTable = table.map(x => x.map((s, i) => s.padEnd(maxLength[i])));
 
-	/**
-	 * 
-	 * @param {number} row 
-	 * @param {number} col 
-	 * @returns {string}
-	 */
-	function getString(row, col){
+
+	function getString(row: number, col: number): string
+	{
 		return paddedTable[row][col] || ''.padStart(maxLength[col]);
 	}
 
